087e372ea292f59747dd7007f2cccf61636fe249
Mark Yoon <yoon@northwestern.edu>
2014-02-28 12:03:47 -0600

    harmonize header generation for cat3 export in UI and rake task. #5

--- app/controllers/api/reports_controller.rb
+++ app/controllers/api/reports_controller.rb
@@ -1,5 +1,5 @@
 module Api
-  
+
   class ReportsController < ApplicationController
     before_filter :authenticate_user!
 skip_authorization_check
@@ -9,39 +9,17 @@ skip_authorization_check
       exporter =  HealthDataStandards::Export::Cat3.new
       effective_date = params["effective_date"] || current_user.effective_date
       end_date = Time.at(effective_date.to_i)
-      render text: exporter.export(HealthDataStandards::CQM::Measure.top_level.where(filter), 
-                                   generate_header, 
-                                   effective_date, 
-                                   end_date.years_ago(1), 
+      render text: exporter.export(HealthDataStandards::CQM::Measure.top_level.where(filter),
+                                   generate_header(Time.now),
+                                   effective_date,
+                                   end_date.years_ago(1),
                                    end_date)
     end
 
     private
-  #This is real ugly and needs to be thought out as to how we generate header information for Cat 3 documents
-    def generate_header
-     header_hash=  {identifier: {root: "header_root", extension: "header_ext"},
-       authors: [{ids: [ {root: "author_root" , extension: "author_extension"}],
-                     device: {name:"dvice_name" , model: "device_mod"},
-                     addresses:[],
-                     telecoms: [],
-                     time: Time.now,
-                     organization: {ids: [ {root: "authors_organization_root" , extension: "authors_organization_ext"}],
-                                    name: ""}}],
-       custodian: {ids: [ {root: "custodian_root" , extension: "custodian_ext"}],
-                   person: {given: "", family: ""},
-                   organization: {ids: [ {root: "custodian_rganization_root" , extension: "custodian_organization_ext"}],
-                                  name: ""}},
-       legal_authenticator:{ids: [ {root: "legal_authenticator_root" , extension: "legal_authenticator_ext"}],
-                            addresses: [],
-                            telecoms:[],
-                            time: Time.now,
-                            person: {given:"hey", family: "there"},
-                            organization:{ids: [ {root: "legal_authenticator_org_root" , extension: "legal_authenticator_org_ext"}],
-                                          name: ""}
-                            }
-        }
-      
-      Qrda::Header.new(header_hash)
+
+    def generate_header(time)
+      Qrda::Header.new(YAML.load(File.read(File.join(Rails.root, 'config', 'qrda3_header.yml'))).deep_merge(legal_authenticator: {time: time}), authors: {time: time})
     end
   end
 end
